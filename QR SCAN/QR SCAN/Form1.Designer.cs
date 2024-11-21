3ce9c07a3ddb3599da07f04d0224c300211ba027
Uyên Bùi <huuuyen1998@gmail.com>
2024-11-21 19:03:06 +0700

    123

---
+++ QR SCAN/QR SCAN/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace QR_SCAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic_cam = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic_cam
+            // 
+            this.pic_cam.Location = new System.Drawing.Point(360, 40);
+            this.pic_cam.Name = "pic_cam";
+            this.pic_cam.Size = new System.Drawing.Size(1043, 852);
+            this.pic_cam.TabIndex = 2;
+            this.pic_cam.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(62, 191);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(263, 180);
+            this.listBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1428, 904);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pic_cam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_cam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic_cam;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QR SCAN/QR SCAN/Form1.cs
@@ -0,0 +1,127 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+
+namespace QR_SCAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MJPEGStream stream;
+
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private Bitmap currentFrame;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Kết nối tới luồng RTSP của camera
+            string rtspUrl = "rtsp://uyen:@Uyen23071998@192.168.1.7:554/cam/realmonitor?channel=4&subtype=0&unicast=true&proto=Onvif";
+            //stream = new MJPEGStream("2");
+            //stream.NewFrame += stream_NewFrame;
+            //stream.Start();
+            //timer1.Enabled = true;
+            //timer1.Start();
+
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            videoSource = new VideoCaptureDevice(videoDevices[3].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+
+            // Bắt đầu phát luồng
+            videoSource.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Bitmap img = (Bitmap)pic_cam.Image;
+            if (img != null)
+            {
+                ZXing.BarcodeReader Reader = new ZXing.BarcodeReader();
+                Result result = Reader.Decode(img);
+                try
+                {
+                    string decoded = result.ToString().Trim();
+                    if (!listBox1.Items.Contains(decoded))
+                    {
+                        listBox1.Items.Insert(0, decoded);
+                    }
+
+                    img.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "");
+                }
+
+            }
+        }
+
+        public void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            pic_cam.Image = bmp;
+        }
+
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            // Lấy khung hình hiện tại
+            pic_cam.Image = (Bitmap)eventArgs.Frame.Clone();
+
+            // Hiển thị hình ảnh lên PictureBox (nếu có giao diện)
+            currentFrame = (Bitmap)eventArgs.Frame.Clone();
+
+            // Quét mã QR
+            ScanQRCode(currentFrame);
+        }
+
+        private void ScanQRCode(Bitmap frame)
+        {
+            try
+            {
+                // Sử dụng ZXing để quét mã QR
+                BarcodeReader reader = new BarcodeReader();
+                var result = reader.Decode(frame);
+
+                if (result != null)
+                {
+                    // Hiển thị nội dung mã QR
+                    Invoke(new Action(() =>
+                    {
+                        //MessageBox.Show($"Mã QR được phát hiện: {result.Text}", "Thông báo");
+                    }));
+
+                    // Dừng luồng video sau khi quét xong (tuỳ chọn)
+                    //videoSource.SignalToStop();
+                }
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine($"Lỗi khi quét mã QR: {ex.Message}");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Dừng video khi đóng form
+            if (videoSource != null && videoSource.IsRunning)
+            {
+                videoSource.SignalToStop();
+            }
+        }
+    }
+}
